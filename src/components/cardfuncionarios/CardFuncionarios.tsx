71ba36c2664d832b7f004a75fc79835d9f790052
Hector Silva Santos <promotoraeliana@yahoo.com.br>
2025-01-28 14:07:59 -0300

    Atualização CardFuncionarios

--- src/components/cardfuncionarios/CardFuncionarios.tsx
+++ src/components/cardfuncionarios/CardFuncionarios.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom"
 
 function CardFuncionarios() {
     return (
+        <>
         <div className='border-slate-900 border 
             flex flex-col rounded overflow-hidden justify-between'>
                 
@@ -12,15 +13,15 @@ function CardFuncionarios() {
                     <img src='https://i.imgur.com/pK6vSCy.png'
                          className='h-12 rounded-full' alt="" />
                     <h3 className='text-lg font-bold text-center uppercase'>
-                        Nome do Usuário
+                        Geandro Araújo
                     </h3>
                 </div>
                 <div className='p-4 '>
                     <h4 className='text-lg font-semibold uppercase'>Titulo</h4>
-                    <p>nome:</p>
-                    <p>Cargo: </p>
-                    <p>Salario: </p>
-                    <p>Empresa:</p>
+                    <p>nome:Geandro Araújo</p>
+                    <p>Cargo: Instrutosr</p>
+                    <p>Salario: 15.000 </p>
+                    <p>Empresa:Generation Brasil</p>
                 </div>
             </div>
             <div className="flex">
@@ -34,6 +35,37 @@ function CardFuncionarios() {
                 </Link>
             </div>
         </div>
+        <div className='border-slate-900 border 
+            flex flex-col rounded overflow-hidden justify-between'>
+                
+            <div>
+                <div className="flex w-full bg-indigo-400 py-2 px-4 items-center gap-4">
+                    <img src='https://i.imgur.com/pK6vSCy.png'
+                         className='h-12 rounded-full' alt="" />
+                    <h3 className='text-lg font-bold text-center uppercase'>
+                        Lorena Belo
+                    </h3>
+                </div>
+                <div className='p-4 '>
+                    <h4 className='text-lg font-semibold uppercase'>Titulo</h4>
+                    <p>nome:Lorena Belo</p>
+                    <p>Cargo: Intrutor Assistente</p>
+                    <p>Salario: 10.000 </p>
+                    <p>Empresa: Generation Brasil</p>
+                </div>
+            </div>
+            <div className="flex">
+                <Link to='' className='w-full text-white bg-indigo-400 
+                    hover:bg-indigo-800 flex items-center justify-center py-2'>
+                    <button>Editar</button>
+                </Link>
+                <Link to='' className='text-white bg-red-400 
+                    hover:bg-red-700 w-full flex items-center justify-center'>
+                    <button>Deletar</button>
+                </Link>
+            </div>
+        </div>
+        </>
     )
 }
 
